81da7d5b973b75f9a2d84b316429bbe3aafffd29
Alexander Reiff <alexander.reiff@me.com>
2016-03-09 05:58:36 +0000

    Cleanup and prune old association imports

--- src/associations/index.js
+++ src/associations/index.js
@@ -1,5 +1,4 @@
 import oneToOne from './one-to-one';
 import oneToMany from './one-to-many';
-import associate from './associate';
 
-export { oneToOne, oneToMany, associate };
+export { oneToOne, oneToMany };
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const register = (server, options = {}, next) => {
   options.scopePrefix = options.scopePrefix || 's';
   options.snakeCase = options.snakeCase || false;
   options.private = options.private || [];
-  
+
   let db = server.plugins['hapi-sequelize'].db;
   let models = db.sequelize.models;
 
@@ -20,7 +20,7 @@ const register = (server, options = {}, next) => {
 
     return reply.continue();
   };
-  
+
   const convertCase = options.snakeCase ? snakeCase : function (str) { return str; };
 
   server.ext({
@@ -38,7 +38,7 @@ const register = (server, options = {}, next) => {
     if (model.options.name.singular !== model.name) continue;
 
     crud(server, model, options);
-    
+
     if (model.name !== 'message') continue;
 
     for (let key of Object.keys(model.associations)) {
@@ -50,61 +50,21 @@ const register = (server, options = {}, next) => {
       association._plural = convertCase(associationName.plural);
       association._singular = convertCase(associationName.singular);
 
-/*
-      let targetAssociations = target.associations[sourceName.plural] || target.associations[sourceName.singular];
-      let sourceType = association.associationType,
-          targetType = (targetAssociations || {}).associationType;
-*/
-
       try {
         switch(associationType) {
           case 'BelongsTo':
-            associations.oneToOne(server, source, association, options);
-            break;          
-/*
           case 'HasOne':
-            associations.oneToOne(server, target, source, options);
+            associations.oneToOne(server, source, association, options);
             break;
           case 'HasMany':
-          case 'BelongsToMany':          
-            associations.oneToMany(server, source, target, options);
+          case 'BelongsToMany':
+            associations.oneToMany(server, source, association, options);
             break;
-*/
         }
-        
-/*
-        if (sourceType === 'BelongsTo') {
-          associations.oneToOne(server, source, target, options);
-          //associations.oneToOne(server, target, source, options);
-        }
-        
-        
-        
-        if (sourceType === 'HasMany') {
-          associations.oneToMany(server, source, target, options);
-        }
-
-        if (sourceType === 'BelongsTo' && targetType === 'HasMany') {
-          associations.oneToOne(server, source, target, options);
-          associations.oneToOne(server, target, source, options);
-          associations.oneToMany(server, target, source, options);
-        }
-
-        if (sourceType === 'BelongsToMany' && targetType === 'BelongsToMany') {
-          associations.oneToOne(server, source, target, options);
-          associations.oneToOne(server, target, source, options);
-
-          associations.oneToMany(server, source, target, options);
-          associations.oneToMany(server, target, source, options);
-        }
-*/
-
-        associations.associate(server, source, target, options);
-//         associations.associate(server, target, source, options);
       } catch(e) {
         // There might be conflicts in case of models associated with themselves and some other
         // rare cases.
-        console.trace(e)
+        console.warm(e)
       }
     }
   }
